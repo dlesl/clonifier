8a7c2b012b4e77109f6124ebc50ade519bfef449
David <dlesl@users.noreply.github.com>
2019-04-23 22:05:11 +0200

    add some documentation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Clonifier
 This is a web app that lets you view DNA sequences in Genbank (`.gb`) format and simulate PCR and DNA assembly methods such as Gibson Assembly.
+
+## Notes
+All of the work is done by the 'backend' which is written in Rust and runs in a web worker. Because of this, every request to the Rust backend is asynchronous and returns a promise (see `app/worker_comms` for details). To keep things feeling snappy and avoiding excessive 'loading' indicators, the frontend makes use of React's upcoming Suspense feature. In short, views can "throw promises" during rendering, and React will delay rendering until the promise resolves.
+
+Once WebAssembly threads become available, it should be possible to avoid most of this complexity!
+
+The code in `/rust` in this repository is mostly just glue code, the 
+actual work is done in [`gb-io`](https://github.com/dlesl/gb-io), [`pcr`](https://github.com/dlesl/pcr) and [`assembly`](https://github.com/dlesl/assembly).
--- app/worker_comms/index.ts
+++ app/worker_comms/index.ts
@@ -1,39 +1,48 @@
 import {
-  ArgType,
-  CallRequest,
-  DeleteRequest,
-  NewRequest,
-  Response,
-  DbgLogObjectsRequest,
-  LogMessage
+    ArgType,
+    CallRequest,
+    DeleteRequest,
+    NewRequest,
+    Response,
+    DbgLogObjectsRequest,
+    LogMessage
 } from "../../worker/shared";
 import * as workerShims from "./worker_shims";
 
 const worker = new Worker("worker.js");
 
+/** This function creates a message handler that stores all messages
+ *  received until `setHandler` is called. At this point, the queued
+ *  messages are passed to the handler and any further messages are
+ *  forwarded directly to the handler. This allows us to have one
+ *  global instance of the worker, while avoiding any dependency on
+ *  the consumer or the need for the consumer to call `init` or
+ *  similar.
+ */
+
 function handlerQueueUntilSet<T>(): [
-  ((e: T) => void),
-  (handler: (e: T) => void) => void
+    ((e: T) => void),
+    (handler: (e: T) => void) => void
 ] {
-  let queue = new Array<T>();
-  let _handler = null;
-  const handler = (e: T) => {
-    if (_handler) {
-      _handler(e);
-    } else {
-      queue.push(e);
-    }
-  };
-  const setHandler = (h: (e: T) => void) => {
-    _handler = h;
-    if (queue) {
-      for (const e of queue) {
-        _handler(e);
-      }
-      queue = null;
-    }
-  };
-  return [handler, setHandler];
+    let queue = new Array<T>();
+    let _handler = null;
+    const handler = (e: T) => {
+        if (_handler) {
+            _handler(e);
+        } else {
+            queue.push(e);
+        }
+    };
+    const setHandler = (h: (e: T) => void) => {
+        _handler = h;
+        if (queue) {
+            for (const e of queue) {
+                _handler(e);
+            }
+            queue = null;
+        }
+    };
+    return [handler, setHandler];
 }
 
 const [onError, setOnError] = handlerQueueUntilSet<string>();
@@ -42,11 +51,14 @@ export const setErrorHandler = setOnError;
 const [onLogMessage, setOnLogMessage] = handlerQueueUntilSet<LogMessage>();
 export const setLogHandler = setOnLogMessage;
 
-// if the worker fails, bail
+// Catches catastrophic errors in the worker:
+// - code parsing/webassembly loading errors
+// - rust panics
+// These errors are non-recoverable and render the worker unusable.
 worker.onerror = ev =>
-  onError(
-    "Worker failed\n" + ev ? `${ev.filename}:${ev.lineno} : ${ev.message}` : ""
-  );
+    onError(
+        "Worker failed\n" + ev ? `${ev.filename}:${ev.lineno} : ${ev.message}` : ""
+    );
 
 // slab allocator containing `[resolve, reject]` for every pending call.
 // data structure implementation stolen from wasm_bindgen :)
@@ -54,84 +66,95 @@ worker.onerror = ev =>
 const calls = [];
 let calls_next = 0;
 
-export function dbgPendingCalls(): number {
-  return calls.filter(v => typeof v !== "number").length;
-}
-
-export function dbgLogObjects() {
-  const req: DbgLogObjectsRequest = { command: "log_objects" };
-  worker.postMessage(req);
-}
-
 function onDone(idx: number, success: boolean, res: any) {
-  const fn = calls[idx][success ? 0 : 1];
-  calls[idx] = calls_next;
-  calls_next = idx;
-  fn(res); // resolve/reject promise
+    const fn = calls[idx][success ? 0 : 1];
+    // remove pending call
+    calls[idx] = calls_next;
+    calls_next = idx;
+    // resolve/reject promise
+    fn(res);
 }
 
+/** How many worker calls have not yet returned? */
+export function dbgPendingCalls(): number {
+    return calls.filter(v => typeof v !== "number").length;
+}
+
+/** Tell the worker to log all existing proxied objects (to
+ * console) */
+export function dbgLogObjects() {
+    const req: DbgLogObjectsRequest = { command: "log_objects" };
+    worker.postMessage(req);
+}
+
+/** Tell the worker to forget an object (note, this doesn't free the
+ * object. You shouldn't call this directly, use `RemoteObj.free`) */
 export function workerDeleteObj(obj: workerShims.RemoteObj) {
-  const req: DeleteRequest = { command: "delete", targetObj: obj.id };
-  worker.postMessage(req);
+    const req: DeleteRequest = { command: "delete", targetObj: obj.id };
+    worker.postMessage(req);
 }
 
+/** Send a response-requiring request to the worker, and return a
+ * Promise that resolves upon its completion. You probably don't want
+ * to call this directly. Intended for use by the shims in
+ * worker_shims.ts */
 export function workerRequest(
-  req: NewRequest | CallRequest,
-  transfer?: Transferable[]
+    req: NewRequest | CallRequest,
+    transfer?: Transferable[]
 ): Promise<any> {
-  if (calls_next === calls.length) {
-    calls.push(calls.length + 1);
-  }
-  const idx = calls_next;
-  calls_next = calls[idx];
-  const p = new Promise((resolve, reject) => (calls[idx] = [resolve, reject])); // callback gets executed before return
-  req.callId = idx;
-  worker.postMessage(req, transfer);
-  return p;
+    if (calls_next === calls.length) {
+        calls.push(calls.length + 1);
+    }
+    const idx = calls_next;
+    calls_next = calls[idx];
+    const p = new Promise((resolve, reject) => (calls[idx] = [resolve, reject])); // callback gets executed before return
+    req.callId = idx;
+    worker.postMessage(req, transfer);
+    return p;
 }
 
 export function workerCall(
-  fn: string,
-  retType: ArgType,
-  args: any[],
-  specialArgs?,
-  transfer?: Transferable[]
+    fn: string,
+    retType: ArgType,
+    args: any[],
+    specialArgs?,
+    transfer?: Transferable[]
 ): Promise<any> {
-  return workerRequest(
-    { fn, args, specialArgs, command: "call", retType },
-    transfer
-  );
+    return workerRequest(
+        { fn, args, specialArgs, command: "call", retType },
+        transfer
+    );
 }
 
 worker.onmessage = e => {
-  const resp: Response = e.data;
-  switch (resp.command) {
-    case "call":
-      {
-        let retVal = resp.retVal;
-        if (resp.retType === "obj") {
-          retVal = new workerShims[resp.targetType](resp.retVal);
-        } else if (resp.retType === "obj_array") {
-          retVal = retVal.map(v => new workerShims[resp.targetType](v));
-        }
-        onDone(resp.callId, true, retVal);
-      }
-      break;
-    case "new":
-      {
-        const retVal = new workerShims[resp.targetType](resp.retVal);
-        onDone(resp.callId, true, retVal);
-      }
-      break;
-    case "error":
-      {
-        onDone(resp.callId, false, resp.error);
-      }
-      break;
-    case "log":
-      {
-        onLogMessage(resp.msg);
-      }
-      break;
-  }
+    const resp: Response = e.data;
+    switch (resp.command) {
+        case "call":
+            {
+                let retVal = resp.retVal;
+                if (resp.retType === "obj") {
+                    retVal = new workerShims[resp.targetType](resp.retVal);
+                } else if (resp.retType === "obj_array") {
+                    retVal = retVal.map(v => new workerShims[resp.targetType](v));
+                }
+                onDone(resp.callId, true, retVal);
+            }
+            break;
+        case "new":
+            {
+                const retVal = new workerShims[resp.targetType](resp.retVal);
+                onDone(resp.callId, true, retVal);
+            }
+            break;
+        case "error":
+            {
+                onDone(resp.callId, false, resp.error);
+            }
+            break;
+        case "log":
+            {
+                onLogMessage(resp.msg);
+            }
+            break;
+    }
 };
